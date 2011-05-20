1edca807dd71c9ab445d172ebb2ac45b8d163869
Tim Felty <Justinius@users.noreply.github.com>
2011-05-19 21:40:27 -0500

    Contains the code for the CreateContour dll, as well as sample applications. The GUI program contains a sample of how one would allow a user to supply a function that the main program can call and run.

---
+++ ContourGui/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace ContourGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ContourBox = new System.Windows.Forms.PictureBox();
+            this.defaultFunctions = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SourceCode = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ErrorText = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MaxBoxX = new System.Windows.Forms.TextBox();
+            this.MinBoxX = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.MaxBoxY = new System.Windows.Forms.TextBox();
+            this.MinBoxY = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ContourBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ContourBox
+            // 
+            this.ContourBox.BackColor = System.Drawing.SystemColors.Window;
+            this.ContourBox.Location = new System.Drawing.Point(124, 2);
+            this.ContourBox.Name = "ContourBox";
+            this.ContourBox.Size = new System.Drawing.Size(500, 500);
+            this.ContourBox.TabIndex = 0;
+            this.ContourBox.TabStop = false;
+            // 
+            // defaultFunctions
+            // 
+            this.defaultFunctions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.defaultFunctions.FormattingEnabled = true;
+            this.defaultFunctions.Items.AddRange(new object[] {
+            "De Jong 1",
+            "Rosenbrock\'s Valley",
+            "Rastrigin\'s Function",
+            "Schwefel\'s Function",
+            "Griewangk\'s Function",
+            "Michalewicz\'s Function",
+            "Easom\'s Function",
+            "Drop Wave Function",
+            "Schubert\'s Function",
+            "User Entered"});
+            this.defaultFunctions.Location = new System.Drawing.Point(316, 508);
+            this.defaultFunctions.Name = "defaultFunctions";
+            this.defaultFunctions.Size = new System.Drawing.Size(121, 21);
+            this.defaultFunctions.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 516);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Function";
+            // 
+            // SourceCode
+            // 
+            this.SourceCode.Location = new System.Drawing.Point(25, 609);
+            this.SourceCode.Name = "SourceCode";
+            this.SourceCode.Size = new System.Drawing.Size(700, 115);
+            this.SourceCode.TabIndex = 6;
+            this.SourceCode.Text = "public class userclass\n{\n     public static double func(double x, double y)\n     " +
+                "{\n          return System.Math.Sqrt(x*x + y*y);\n      }\n}";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 593);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(330, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter Your Own Function - Replace the body of func with your code.";
+            // 
+            // ErrorText
+            // 
+            this.ErrorText.Location = new System.Drawing.Point(25, 743);
+            this.ErrorText.Name = "ErrorText";
+            this.ErrorText.Size = new System.Drawing.Size(700, 73);
+            this.ErrorText.TabIndex = 6;
+            this.ErrorText.TabStop = false;
+            this.ErrorText.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 727);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Errors";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 508);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 21);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Draw Contour";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MaxBoxX
+            // 
+            this.MaxBoxX.Location = new System.Drawing.Point(142, 551);
+            this.MaxBoxX.Name = "MaxBoxX";
+            this.MaxBoxX.Size = new System.Drawing.Size(89, 20);
+            this.MaxBoxX.TabIndex = 2;
+            // 
+            // MinBoxX
+            // 
+            this.MinBoxX.Location = new System.Drawing.Point(271, 551);
+            this.MinBoxX.Name = "MinBoxX";
+            this.MinBoxX.Size = new System.Drawing.Size(89, 20);
+            this.MinBoxX.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(139, 535);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Max Val For X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(268, 535);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Min Val For X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(388, 535);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Max Val For Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(496, 535);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Min Val For Y";
+            // 
+            // MaxBoxY
+            // 
+            this.MaxBoxY.Location = new System.Drawing.Point(391, 551);
+            this.MaxBoxY.Name = "MaxBoxY";
+            this.MaxBoxY.Size = new System.Drawing.Size(89, 20);
+            this.MaxBoxY.TabIndex = 4;
+            // 
+            // MinBoxY
+            // 
+            this.MinBoxY.Location = new System.Drawing.Point(499, 551);
+            this.MinBoxY.Name = "MinBoxY";
+            this.MinBoxY.Size = new System.Drawing.Size(89, 20);
+            this.MinBoxY.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 832);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MinBoxY);
+            this.Controls.Add(this.MinBoxX);
+            this.Controls.Add(this.MaxBoxY);
+            this.Controls.Add(this.MaxBoxX);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ErrorText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SourceCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.defaultFunctions);
+            this.Controls.Add(this.ContourBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ContourBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ContourBox;
+        private System.Windows.Forms.ComboBox defaultFunctions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox SourceCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox ErrorText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox MaxBoxX;
+        private System.Windows.Forms.TextBox MinBoxX;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox MaxBoxY;
+        private System.Windows.Forms.TextBox MinBoxY;
+    }
+}
+
---
+++ ContourGui/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using System.Reflection; 
+using CreateContour;
+
+namespace ContourGui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //double DeJongX_max = 6, DeJongX_min = -6;
+            //double RosenbrockX_max = 2, RosenbrockX_min = -2;
+            //double RastriginX_max = 4, RastriginX_min = -4;
+            //double SchwefelX_max = 100, SchwefelX_min = -100;
+            //double GriewangkX_max = 10, GriewangkX_min = -10;
+            //double MichalewiczX_max = 4, MichalewiczX_min = 0;
+            //double EasomX_max = 20, EasomX_min = -20;
+            //double DropWaveX_max = 1, DropWaveX_min = -1;
+            //double SchubertX_max = 4, SchubertX_min = -4;
+
+            if (MaxBoxX.Text.Equals(""))
+                MaxBoxX.Text = "100";
+            if (MinBoxX.Text.Equals(""))
+                MinBoxX.Text = "-100";
+
+            if (MaxBoxY.Text.Equals(""))
+                MaxBoxY.Text = "100";
+            if (MinBoxY.Text.Equals(""))
+                MinBoxY.Text = "-100";
+
+            double maxX_v = double.Parse(MaxBoxX.Text);
+            double minX_v = double.Parse(MinBoxX.Text);
+            double maxY_v = double.Parse(MaxBoxY.Text);
+            double minY_v = double.Parse(MinBoxY.Text);
+
+            if (maxX_v == minX_v)
+                maxX_v = minX_v + 100;
+            if (maxX_v < minX_v)
+            {
+                double temp = maxX_v;
+                maxX_v = minX_v;
+                minX_v = temp;
+            }
+
+            if (maxY_v == minY_v)
+                maxY_v = minY_v + 100;
+            if (maxY_v < minY_v)
+            {
+                double temp = maxY_v;
+                maxY_v = minY_v;
+                minY_v = temp;
+            }
+
+            Contour this_contour = new Contour(maxX_v, minX_v, maxY_v, minY_v, 500, 500);
+                    
+            //De Jong 1
+            //Rosenbrock's Valley
+            //Rastrigin's Function
+            //Schwefel's Function
+            //Griewangk's Function
+            //Michalewicz's Function
+            //Easom's Function
+            //Drop Wave Function
+            //Schubert's Function
+            //User Entered
+            switch(defaultFunctions.SelectedIndex)
+            {
+                case 0:
+                    this_contour.create_contour(Program.DeJong);
+                    break;
+                case 1:
+                    this_contour.create_contour(Program.Rosenbrock);
+                    break;
+                case 2:
+                    this_contour.create_contour(Program.Rastrigin);
+                    break;
+                case 3:
+                    this_contour.create_contour(Program.Schwefel);
+                    break;
+                case 4:
+                    this_contour.create_contour(Program.Griewangk);
+                    break;
+                case 5:
+                    this_contour.create_contour(Program.Michalewicz);
+                    break;
+                case 6:
+                    this_contour.create_contour(Program.Easom);
+                    break;
+                case 7:
+                    this_contour.create_contour(Program.DropWave);
+                    break;
+                case 8:
+                    this_contour.create_contour(Program.Schubert);
+                    break;
+                case 9:
+                    CodeDomProvider codeProvider = CodeDomProvider.CreateProvider("CSharp");
+                    CompilerParameters parameters = new CompilerParameters();
+                    parameters.GenerateInMemory = true;
+                    
+                    string SourceString = SourceCode.Text;
+                    CompilerResults results = codeProvider.CompileAssemblyFromSource(parameters, SourceString);
+                    
+                    string error_string = "";
+                    if (results.Errors.Count > 0)
+                    {
+                        foreach (CompilerError CompErr in results.Errors)
+                        {
+                            error_string = String.Format("{0} \n Line number " + CompErr.Line + ", Error Number: " + CompErr.ErrorNumber + ", '" + CompErr.ErrorText + ";", error_string);
+                        }
+                        ErrorText.Text = error_string;
+                        return;
+                    }
+
+                    Assembly mAssembly = results.CompiledAssembly;
+                    
+                    #region error checking
+                    MethodInfo[] user_meth = mAssembly.GetType("userclass").GetMethods(BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static);
+                    if (user_meth.Length != 1)
+                    {
+                        ErrorText.Text = "Either no class: \"userclass\" or no functions were defined.";
+                        return;
+                    }
+
+                    string func_name = user_meth[0].Name;
+
+                    ParameterInfo[] param = user_meth[0].GetParameters();
+                    if (param.Length != 2)
+                    {
+                        ErrorText.Text = "Function has an incorrect number of parameters: Should be 2.";
+                        return;
+                    }
+
+                    if (param[0].ParameterType != Type.GetType("System.Double") || param[1].ParameterType != Type.GetType("System.Double"))
+                    {
+                        ErrorText.Text = "Function requires parameters of type double.";
+                        return;
+                    }
+
+                    if (user_meth[0].ReturnType != Type.GetType("System.Double"))
+                    {
+                        ErrorText.Text = "Function returns an incorrect data type: Should be double.";
+                        return;
+                    }
+
+                    if (user_meth[0].ReturnType != Type.GetType("System.Double"))
+                    {
+                        ErrorText.Text = "Function returns an incorrect data type: Should be double.";
+                        return;
+                    }
+                    #endregion
+
+                    Type script = mAssembly.GetType("userclass");
+                    this_contour.create_contour(ref script, ref func_name);
+ 
+                    break;
+                default:
+                    ErrorText.Text = "Nothing Selected.";
+                    break;
+            }
+
+
+            ContourBox.Image = this_contour.contour_bmp;
+        }
+
+                
+    }
+}
